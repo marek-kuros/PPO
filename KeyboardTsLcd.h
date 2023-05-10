e9ef8a7b39e130a16556ece1368d2cb501b68c98
marek-kuros <104457284+marek-kuros@users.noreply.github.com>
2023-05-10 12:34:31 +0200

    Create KeyboardTsLcd.h

---
+++ KeyboardTsLcd.h
@@ -0,0 +1,15 @@
+#ifndef KEYBOARDTSLCD
+#define KEYBOARDTSLCD
+
+#include "Keyboard_Ts.h"
+#include "Led_Lcd.h"
+
+class KeyboardTsLcd: public Keyboard_Ts, public Led_Lcd{
+    public:
+        Keyboard_Ts *pKeyboard;
+        Led_Lcd *pLed;
+        KeyboardTsLcd(unsigned char);
+        void eRead();
+};
+
+#endif
