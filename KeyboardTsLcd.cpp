f6aff635126ca240f0bfd80b780b7b199a549e21
marek-kuros <104457284+marek-kuros@users.noreply.github.com>
2023-05-10 13:45:49 +0200

    Update KeyboardTsLcd.cpp

--- KeyboardTsLcd.cpp
+++ KeyboardTsLcd.cpp
@@ -1,30 +1,38 @@
 #include "KeyboardTsLcd.h"
 
+#define INVERSION 3
+
 KeyboardTsLcd::KeyboardTsLcd(unsigned char _ucColumn)
 {
     pKeyboard = new Keyboard_Ts(_ucColumn);
     pLed = new Led_Lcd(_ucColumn);
+    pKeyboardTsLcd = new Led_Lcd(3);
+    pLed->On(4);
 }
 
 void KeyboardTsLcd::eRead(){
-    switch(pKeyboard->eRead()){
+    uint8_t ui8LedInvert;
+    switch (pKeyboard->eRead()) {
         case BUTTON_0:
-            pLed->On(0);
+            ui8LedInvert = 0;
             break;
 
         case BUTTON_1:
-            pLed->On(1);
+            ui8LedInvert = 1;
             break;
 
         case BUTTON_2:
-            pLed->On(2);
+            ui8LedInvert = 2;
             break;
 
         case BUTTON_3:
-            pLed->On(3);
+            ui8LedInvert = 3;
             break;
+
         default:
-            pLed->On(4);
+            ui8LedInvert = 4;
             break;
         }
+        
+        pKeyboardTsLcd->On(INVERSION - ui8LedInvert);
 }
