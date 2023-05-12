68072a845e553cb2672054241c451f470c8e3a8c
marek-kuros <104457284+marek-kuros@users.noreply.github.com>
2023-05-12 12:06:14 +0200

    Update Led_Lcd.cpp

--- Led_Lcd.cpp
+++ Led_Lcd.cpp
@@ -1,26 +1,34 @@
 #include "Led_Lcd.h"
+#include "stm32f429i_discovery_lcd.h"
 
 #define X_SIZE_OF_BUTTON 80
 #define Y_SIZE_OF_BUTTON 80
 #define SIZE_OF_FRAME 1
 #define STARTING_POINT 0
 #define BUTTON_MULTIPY  80
+#define LED_NO 4
 
 Led_Lcd::Led_Lcd(uint8_t ui8ColumnIndexLed){
     ui8ColumnOffsetLed = ui8ColumnIndexLed;
     Init();
     Clear(LCD_COLOR_BLACK);
     SetFont(&Font24);
+    //On(4);
 }
 
 void Led_Lcd::On(uint8_t uIndexOfLed){
-    for(uint8_t uButtonIndex = 0; uButtonIndex < 4; uButtonIndex++){           
-        SetTextColor(uButtonIndex == uIndexOfLed ? LCD_COLOR_GREEN : LCD_COLOR_BLUE);
-        FillRect(STARTING_POINT + (ui8ColumnOffsetLed - 1)*X_SIZE_OF_BUTTON, SIZE_OF_FRAME + uButtonIndex*BUTTON_MULTIPY, X_SIZE_OF_BUTTON, Y_SIZE_OF_BUTTON);
-        SetTextColor(LCD_COLOR_GREEN);
-        DrawRect(STARTING_POINT + (ui8ColumnOffsetLed - 1)*X_SIZE_OF_BUTTON, uButtonIndex*BUTTON_MULTIPY, X_SIZE_OF_BUTTON, Y_SIZE_OF_BUTTON);
-        SetTextColor(LCD_COLOR_WHITE);
-        SetBackColor(LCD_COLOR_RED);
-        DisplayChar(STARTING_POINT + (ui8ColumnOffsetLed - 1)*X_SIZE_OF_BUTTON, SIZE_OF_FRAME + uButtonIndex*BUTTON_MULTIPY, '0' + uButtonIndex+1);
+    for(uint8_t uButtonIndex = 0; uButtonIndex <= LED_NO; uButtonIndex++){
+        Draw(uButtonIndex, LCD_COLOR_BLUE);
     }
+    Draw(uIndexOfLed, LCD_COLOR_GREEN);
+}
+
+void Led_Lcd::Draw(uint8_t uButtonIndex, uint32_t uiNextColour){     
+    SetTextColor(uiNextColour);
+    FillRect(STARTING_POINT + (ui8ColumnOffsetLed - 1)*X_SIZE_OF_BUTTON, SIZE_OF_FRAME + uButtonIndex*BUTTON_MULTIPY, X_SIZE_OF_BUTTON, Y_SIZE_OF_BUTTON);
+    SetTextColor(LCD_COLOR_GREEN);
+    DrawRect(STARTING_POINT + (ui8ColumnOffsetLed - 1)*X_SIZE_OF_BUTTON, uButtonIndex*BUTTON_MULTIPY, X_SIZE_OF_BUTTON, Y_SIZE_OF_BUTTON);
+    SetTextColor(LCD_COLOR_WHITE);
+    SetBackColor(LCD_COLOR_RED);
+    DisplayChar(STARTING_POINT + (ui8ColumnOffsetLed - 1)*X_SIZE_OF_BUTTON, SIZE_OF_FRAME + uButtonIndex*BUTTON_MULTIPY, '0' + uButtonIndex+1);
 }
