d50145df0b7fb3478e23e47430511127ea89e0f9
marek-kuros <104457284+marek-kuros@users.noreply.github.com>
2023-05-12 12:04:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include "mbed.h"
 #include "KeyboardTsLcd.h"
-
-#define INVERSION 3
+#include "Led_Lcd.h"
+#include "Keyboard_Ts.h"
+#include "stm32f429i_discovery_lcd.h"
 
 int main()
 {
-    KeyboardTsLcd Keyboard(1), Keyboard_3(3);
+    KeyboardTsLcd Keyboard(1);
     while(1){
-        Keyboard.pLed->On(4);
-        Keyboard_3.pLed->On(INVERSION - (Keyboard.pKeyboard->eRead()));
+        Keyboard.eRead();
         wait(0.1);
     }
 }
