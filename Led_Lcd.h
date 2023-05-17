6200552380798e9c35552206660f754120b1a1c2
marek-kuros <104457284+marek-kuros@users.noreply.github.com>
2023-05-17 12:32:22 +0200

    Update Led_Lcd.h

--- Led_Lcd.h
+++ Led_Lcd.h
@@ -1,11 +1,11 @@
 #ifndef LED_LCD
 #define LED_LCD
 #include "LCD_DISCO_F429ZI.h"
-#include <cstdint>
 
 class Led_Lcd: public LCD_DISCO_F429ZI{
     private:
         uint8_t ui8ColumnOffsetLed;
+        void Draw(uint8_t, uint32_t);
     public:
         Led_Lcd(uint8_t = 1);
         void On(uint8_t);
