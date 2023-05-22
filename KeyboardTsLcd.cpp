4acd2d42b3f010cebc8f60502bdbe14229ade6b4
marek-kuros <104457284+marek-kuros@users.noreply.github.com>
2023-05-22 10:53:51 +0200

    Update KeyboardTsLcd.cpp

--- KeyboardTsLcd.cpp
+++ KeyboardTsLcd.cpp
@@ -7,31 +7,33 @@ KeyboardTsLcd::KeyboardTsLcd(unsigned char _ucColumn)
     pLed = new Led_Lcd(_ucColumn);
 }
 
-void KeyboardTsLcd::eRead(){
-    uint8_t ui8LedInvert;
+enum eButton KeyboardTsLcd::eRead(){
     switch (pKeyboard->eRead()){
         case BUTTON_0:
-            ui8LedInvert = 0;
+            pLed->On(0);
+            return BUTTON_0;
             break;
 
         case BUTTON_1:
-            ui8LedInvert = 1;
+            pLed->On(1);
+            return BUTTON_1;
             break;
 
         case BUTTON_2:
-            ui8LedInvert = 2;
+            pLed->On(2);
+            return BUTTON_2;
             break;
 
         case BUTTON_3:
-            ui8LedInvert = 3;
+            pLed->On(3);
+            return BUTTON_3;
             break;
         case RELEASED:
-            ui8LedInvert = 4;
+            pLed->On(4);
+            return RELEASED;
             break;
 
         default:
-            ui8LedInvert = 4;
             break;
         }
-        pLed->On(ui8LedInvert);
 }
